597af787a15d997f4f20c8107af950be3e5018d9
James Polera <j@polera.org>
2010-08-30 00:28:19 -0400

    - Updated connect method to remove unnecessary ehlo. - Added get_ehlo method - Added parse_ehlo method (returns a list of ehlo options) - Added get_server_supports_tls method (bool based on existence of ehlo option STARTTLS) - Added get_max_message_size method (returns max message size in MB based on ehlo option SIZE) - Added the following properties:   * server_max_message_size = get_max_message_size   * server_supports_tls = get_server_supports_tls   * ehlo_options = parse_ehlo   * ehlo = get_ehlo
    - Removed close to socket.close() as to not break other newly added methods.  The Python GC should clean this up.

--- SMTPServerTest.py
+++ SMTPServerTest.py
@@ -4,28 +4,55 @@ import socket
 socket.setdefaulttimeout(1.5)
 
 class SMTPServerTest(object):
-  PORTS = [25,]
-  MESSAGE = 'ehlo'
+  PORTS = [25]
+  MESSAGE = 'ehlo listbot.org\n'
   
   def __init__(self, server_name, additional_ports=None):
     if additional_ports:
         self.PORTS.extend(additional_ports)
-    self.server = server_name
-    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self._tried = False
-    self._results = {}
-     
+    self.server         = server_name
+    self.sock           = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self._tried         = False
+    self._results       = {}
+    self._ehlo_port     = None
+    self._ehlo_response = None
+
+  def get_max_message_size(self):
+    size_option = filter(lambda x: x.split(" ")[0] == "250-SIZE",self.ehlo_options)[0]
+    size_bytes = size_option.split(" ")[1]
+    return long(size_bytes)/1024.0/1024.0
+  server_max_message_size = property(get_max_message_size)
+    
+  def get_server_supports_tls(self):
+    return "250-STARTTLS" in self.ehlo_options
+  server_supports_tls = property(get_server_supports_tls)
+  
+  def parse_ehlo(self):
+    if not self._ehlo_response:
+      self._ehlo_response = self.get_ehlo()
+    return filter(lambda x: x[:4] == '250-',self._ehlo_response.split("\r\n"))[1:]
+  ehlo_options = property(parse_ehlo)
+           
+  def get_ehlo(self):
+    self.connect()
+    try:
+      self.sock.send(self.MESSAGE)
+      return self.sock.recv(1024)
+    except Exception, e:
+      print str(e)
+      return "ehlo failed."
+  ehlo = property(get_ehlo)
+    
   def connect(self):
     if self._tried:
         return self._results
     for port in self.PORTS:
       try:
         self.sock.connect((self.server, port))
-        self.sock.send(self.MESSAGE)
         self._results[port] = {'connected':True, 'message':unicode(self.sock.recv(1024))}
+        self._ehlo_port     = port
       except socket.error:
         self._results[port] = {'connected':False, 'message':u'Connection failed'}
-      self.sock.close()
     self._tried = True
     return self._results
 
@@ -35,9 +62,11 @@ class SMTPServerTest(object):
   results = property(connect)
   
 if __name__ == "__main__":
-  server_list = ['mail.uncryptic.com',
-                 'smtp.gmail.com']
+  server_list = ['smtp.gmail.com']
   for server in server_list:
     print server
     s = SMTPServerTest(server)
     print s.results
+    print("EHLO options %s" % ", ".join(s.ehlo_options))
+    print("TLS Supported? %s" % s.server_supports_tls)
+    print("Max message size: %d MB" % s.server_max_message_size)
